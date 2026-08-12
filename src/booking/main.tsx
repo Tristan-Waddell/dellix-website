04720958673869fd86076654ed7b1049f93fb9ef
Tristan Waddell <tristancwaddell@gmail.com>
2026-08-12 17:28:28 -0400

    Add Vercel Web Analytics

--- src/booking/main.tsx
+++ src/booking/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import '../index.css'
 import { BookingPage } from './BookingPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BookingPage />
+    <Analytics />
   </StrictMode>,
 )
